d88696bdff56cdc091bcccad3107c17223cb4017
SaefudinZuhri1 <zuhrisaeff12@gmail.com>
2023-03-16 00:18:19 +0700

    Metode pop

--- List/Remove List.py
+++ List/Remove List.py
@@ -5,5 +5,11 @@ print(listBaruLagi)
 
 # Menghapus index tertentu
 list11 = ["Jakarta", "Yogyakarta", "Jawa"]
-list11.pop(2)
+list11.pop(1)
 print(list11)
+
+# Jika metode pop tidak ditentukan maka akan menghapus item terakhir
+# Menghapus index tertentu
+list12 = ["Jakarta", "Yogyakarta", "Jawa"]
+list12.pop()
+print(list12)

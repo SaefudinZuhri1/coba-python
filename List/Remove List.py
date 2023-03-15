57926f01ed8514696b605fa523705071a763678e
SaefudinZuhri1 <zuhrisaeff12@gmail.com>
2023-03-16 00:16:30 +0700

    Menambahkan materi menghapus index

--- List/Remove List.py
+++ List/Remove List.py
@@ -2,3 +2,8 @@
 listBaruLagi = ["UPI", "UNJ", "UNY"]
 listBaruLagi.remove("UNY")
 print(listBaruLagi)
+
+# Menghapus index tertentu
+list11 = ["Jakarta", "Yogyakarta", "Jawa"]
+list11.pop(2)
+print(list11)

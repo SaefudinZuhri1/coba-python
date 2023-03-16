146ea3336a52f9e39065ea3b78570b4c05663161
SaefudinZuhri1 <zuhrisaeff12@gmail.com>
2023-03-16 11:25:01 +0700

    Loop List

---
+++ List/Loop List.py
@@ -0,0 +1,9 @@
+listBaru = ["Jeruk", "Mangga", "Pepaya"]
+for x in listBaru:
+	print(listBaru)
+
+
+# Loop melalui nomor index
+listBaru1 = ["Jakarta", "Jakarta Kota", "Jakarta Ibukota"]
+for i in range(len(listBaru1)):
+	print(listBaru1[i])

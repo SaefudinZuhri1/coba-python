ad751fb685b50185897f8d8f95b1b203c0736772
SaefudinZuhri1 <zuhrisaeff12@gmail.com>
2023-03-16 21:59:53 +0700

    Menambahkan materi mengurutkandaftar secara menurun

--- List/Mengurutkan Daftar.py
+++ List/Mengurutkan Daftar.py
@@ -8,3 +8,14 @@ print(listBuah)
 listAngka = [60, 50, 10, 20, 1000]
 listAngka.sort()
 print(listAngka)
+
+# Mengurutkan daftar secara menurun
+# Menggunakan argumen reverse = True
+namaKota = ["Jakarta", "Bekasi", "Tangerang", "Bogor", "Tangerang Selatan"]
+namaKota.sort(reverse = True)
+print(namaKota)
+
+# Mengurutkan angka secara menurun
+urutAngka = [200, 100, 67, 83, 10]
+urutAngka.sort(reverse = True)
+print(urutAngka)

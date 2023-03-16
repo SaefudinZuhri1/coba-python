b8bf8a4730880970ed9da8747ba8bcb1859b0b69
SaefudinZuhri1 <zuhrisaeff12@gmail.com>
2023-03-16 11:28:55 +0700

    Menambahkan materi while loop

--- List/Loop List.py
+++ List/Loop List.py
@@ -7,3 +7,12 @@ for x in listBaru:
 listBaru1 = ["Jakarta", "Jakarta Kota", "Jakarta Ibukota"]
 for i in range(len(listBaru1)):
 	print(listBaru1[i])
+
+# Menggunakan While Loop
+# len untuk menentukan panjang list
+listBaru2 = ["Bandung", "Cimahi", "Garut"]
+x = 0
+while x < len(listBaru2):
+	print(listBaru2[x])
+	x = x + 1
+	

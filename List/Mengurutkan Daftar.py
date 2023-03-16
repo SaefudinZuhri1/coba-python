f3d34678cb5a2d27ae4a1cbf1753070e1f4357d3
SaefudinZuhri1 <zuhrisaeff12@gmail.com>
2023-03-16 21:52:57 +0700

    Menambahkan materi baru Mengurutkan Daftar

---
+++ List/Mengurutkan Daftar.py
@@ -0,0 +1,10 @@
+# Mengurutkan daftar berdasarkan huruf
+# Menggunakan metode sort()
+listBuah = ["Jeruk", "Apel", "Mangga", "Nanas"]
+listBuah.sort()
+print(listBuah)
+
+# Mengurutkan daftar lewat angka
+listAngka = [60, 50, 10, 20, 1000]
+listAngka.sort()
+print(listAngka)

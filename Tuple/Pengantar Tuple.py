d80cb1047825ad4b90e7a505e5900a9c54c9b84c
SaefudinZuhri1 <zuhrisaeff12@gmail.com>
2023-03-19 23:16:09 +0700

    Menambahkan materi baru Tuple Python

---
+++ Tuple/Pengantar Tuple.py
@@ -0,0 +1,8 @@
+# Tuple digunakan untuk menyimpan banyak item dalam satu list
+# Tuple adalah tipe data python yang digunakan untuk menyimpan kumpulan data
+# Tuple merupakan koleksi yang dipesan dan tidak dapan diubah
+# Tuple ditulis dengan tanda kurung bulat
+tupleContoh = ("Jambi", "Aceh", "Medan")
+print(tupleContoh)
+
+

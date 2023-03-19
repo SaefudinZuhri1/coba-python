93c6aaa864545ddc1dce7e67ded2f0faceffc89a
SaefudinZuhri1 <zuhrisaeff12@gmail.com>
2023-03-19 22:48:15 +0700

    Materi join list 2

--- List/Join List.py
+++ List/Join List.py
@@ -5,3 +5,11 @@ listHuruf = ["A", "B", "C"]
 listAngka = [0, 1, 2, 3]
 listGabungan = listHuruf + listAngka
 print(listGabungan)
+
+# Cara kedua adalah menambah semua item di list 1 kedalam list 2 satu per satu
+list1 = ["D", "E", "F", "G"]
+list2 = [0, 1, 2, 3]
+
+for z in list2:
+	list1.append(z)
+print(list1)

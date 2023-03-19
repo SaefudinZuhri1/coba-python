5763f64b18cf427c3814f070e6ad7f7a1278b9a7
SaefudinZuhri1 <zuhrisaeff12@gmail.com>
2023-03-19 22:45:53 +0700

    Menambahkan materi join list python

---
+++ List/Join List.py
@@ -0,0 +1,7 @@
+# Menggabungkan dua daftar
+# Dapat menggunakan operator + untuk menggabungkan dua list
+
+listHuruf = ["A", "B", "C"]
+listAngka = [0, 1, 2, 3]
+listGabungan = listHuruf + listAngka
+print(listGabungan)

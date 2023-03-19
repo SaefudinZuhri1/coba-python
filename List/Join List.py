cfca9bf4dc27725baf349ab80bac782bc62e735e
SaefudinZuhri1 <zuhrisaeff12@gmail.com>
2023-03-19 22:51:09 +0700

    Menambahkan materi join list python menggunakan metode extend

--- List/Join List.py
+++ List/Join List.py
@@ -7,9 +7,22 @@ listGabungan = listHuruf + listAngka
 print(listGabungan)
 
 # Cara kedua adalah menambah semua item di list 1 kedalam list 2 satu per satu
+# Menggunakan metode append
 list1 = ["D", "E", "F", "G"]
 list2 = [0, 1, 2, 3]
 
 for z in list2:
 	list1.append(z)
 print(list1)
+
+# Menggunakan metode extend
+list3 = ["A", "I", "U", "E", "O"]
+list4 = ["B", "C", "D", "F", "G"]
+
+list3.extend(list4)
+print(list3)
+
+
+
+
+

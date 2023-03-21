8d9e61dca4fb07a70785d948f67e181a3196c7ad
SaefudinZuhri1 <zuhrisaeff12@gmail.com>
2023-03-21 23:58:23 +0700

    Menambahkan materi baru set Python

---
+++ Sets/Pengantar Sets Python.py
@@ -0,0 +1,16 @@
+# Set Python
+# Sintaks: mysets = {"value1", "value2", "value3"}
+
+# A. Set
+# Set digunakna untuk menyimpan beberapa item dalam satu variabel.
+# Set adalah salah satu dari 4 tipe data bwaan di Python yang gunanya untuk menyimpan kumpulan data. 3 tipe data lainnya adalah List, Tuple, dan Dictionary, semua mempunyai kualitas dan penggunaan yang berbeda.
+
+# Set adalah kumpulan yang unordered, unchangeable*, dan unindexed.
+# Catatan: Set item tidak dapat diubah, tetapi dapat menghapus item dan menambahkan item baru.
+
+# Contoh: Buat Set
+iniSet = {"Apel", "Mangga", "Jeruk"}
+print(iniSet)
+# Catatan: Set tidak diurutkan, jadi tidak dapat dipastikan urutan yang akan muncul
+
+

75b2b2f3ef66ce28993d7fbde8d3bd966ce3e72f
SaefudinZuhri1 <zuhrisaeff12@gmail.com>
2023-03-21 23:26:30 +0700

    Menambahkan materi join tuple

---
+++ Tuple/Join Tuples.py
@@ -0,0 +1,17 @@
+# Gabung Tuple
+
+# A. Gabungkan Dua Tuple
+# Untuk menggabungkan dua tuple atau lebih, bisa menggunakan operator +
+# Contoh menggabungkan dua tuple:
+iniHuruf = ("a", "b", "c")
+iniAngka = (1, 2, 3)
+
+gabungan = iniHuruf + iniAngka
+print(gabungan)
+
+# Melipatkgandakan Tuple
+# Jika ingin mengalikan konten tuple beberapa kali, dapat menggunakan * operator
+# Contoh: Menggandakan tuple dengan 4
+hewan = ("Ayam", "Anjing", "Unta")
+gandakan = hewan * 4
+print(gandakan)

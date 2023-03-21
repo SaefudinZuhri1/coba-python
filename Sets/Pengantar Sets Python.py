4b3de23d95d830f55539a2a1773352744ac7eb99
SaefudinZuhri1 <zuhrisaeff12@gmail.com>
2023-03-22 02:05:59 +0700

    Menambahkan file pelajaran terakhir tentang pengantar set python

--- Sets/Pengantar Sets Python.py
+++ Sets/Pengantar Sets Python.py
@@ -29,3 +29,50 @@ print(iniSet)
 # Contoh: Nilai duplikat akan diabaikan
 kota = {"Jakarta", "Dumay", "Jambi", "Jakarta"}
 print(kota)
+
+## Catatan: Nilai True dan 1 dianggap sebagai nilai yang sama dalam kumpulan, dan diperlakukan sebagai duplikat.
+# Contoh: True dan 1 dianggap nilai yang sama
+iniSet1 = {"Mangga", "Jeruk", "Monas", True, 1, 3}
+print(iniSet1)
+
+# F. Dapatkan Panjang Set
+# Untuk menentukan berapa banyak item yang dimiliki suatu set, gunakan function len()
+# Contoh: Dapatkan jumlah item dalam satu set
+panjangSet = {"Markisa", "Melon", "Mangga"}
+print(len(panjangSet))
+
+# G. Tetapkan Item –
+# Set item dapat berupa tipe data apapun
+# Contoh: Tipe data string, boolean, float, integer
+tipeString = {"ABA", "APA", "AJA"}
+tipeNumber = {1, 2, 8, 0.2, 1.9}
+tipeBool = {True, False, True, True}
+
+print(tipeString)
+print(tipeNumber)
+print(tipeBool)
+
+# Satu set dapat berisi tipe data yang berbeda
+# Contoh: Satu set berisi string, float, int, boolean
+gabunganSet = {"ABJAD", 20, True, 0.2, "male"}
+print(gabunganSet)
+
+# H. type()
+# Dari prespektif Python, set didefinisikan sebagai objek dengan tipe data 'set'
+# Sintaks <class 'set'>
+# Contoh: Apa tipe data ini?
+tebakTipeData = {"Benar", "Salah", 08.2}
+print(type(tebakTipeData))
+
+# I. Konstruktor Set()
+# DImungkinkan juga untuk menggunakan konstruktor set() untuk membuat set
+# Contoh: Menggunakan konstruktor set() untuk membuat set
+setBuatan = set(("Tomat", "Wortel", "Kol")) # Digabung dengan dua tanda kurung
+print(setBuatan)
+
+# Saat memilih tipe koleksi, akan sangat berguna untuk memahami properti dari tipe data tersebut. Memilih jenis yang tepat untuk kumpulan data tertentu dapat berarti retensi makna, dan dapat berarti peningkatan efisiensi atau keamanan.
+
+
+
+
+

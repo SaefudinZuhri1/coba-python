4bf184fd1a88202b0eb70bf219a9e671dd699899
SaefudinZuhri1 <zuhrisaeff12@gmail.com>
2023-03-22 01:53:15 +0700

    Menambahkan file materi tentang pengantar sets python

--- Sets/Pengantar Sets Python.py
+++ Sets/Pengantar Sets Python.py
@@ -13,4 +13,19 @@ iniSet = {"Apel", "Mangga", "Jeruk"}
 print(iniSet)
 # Catatan: Set tidak diurutkan, jadi tidak dapat dipastikan urutan yang akan muncul
 
+# B. Tetapkan Item
+# Set item tidak diurutkan, tidak dapat diubah, dan tidak mengizinkan duplikat
 
+# C. Tidak dipesan
+# Unordered artinya item dalam satu set tidak memiliki urutan yang ditentukan.
+# Set item dapat muncul dalam urutan yang berbeda setiap kali menggunakannya, dan tidak dapat dirujuk dengan indeks atau kunci.
+
+# D. Tidak dapat diubah
+# Item set tidak dapat diubah, artinya tidak dapat mengubah item setelah set dibuat.
+## Setelah set dibuat, kita tidak bisa merubah itemnya, tetapi dapat menghapus dan menambahkan item baru.
+
+# E. Duplikat Tidak Diizinkan
+# Set tidak boleh memiliki dua item dengan nilai yang sama.
+# Contoh: Nilai duplikat akan diabaikan
+kota = {"Jakarta", "Dumay", "Jambi", "Jakarta"}
+print(kota)

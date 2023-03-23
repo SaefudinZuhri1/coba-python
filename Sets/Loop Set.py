745eac6829facfda2eec191ee3322c939863d127
SaefudinZuhri1 <zuhrisaeff12@gmail.com>
2023-03-23 21:37:47 +0700

    Menambahkan materi tentang Loop Python

---
+++ Sets/Loop Set.py
@@ -0,0 +1,8 @@
+# Sets Loop
+
+# A. Item Loop
+# Kita dapat mengulangi item yang ditetapkan dengan menggunakan for loop
+# Contoh: Ulangi set, dan cetak nilainya
+iniBuah = {"Apel", "Mangga", "Jeruk"}
+for a in iniBuah:
+  print(a)
